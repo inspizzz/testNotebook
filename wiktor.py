a78fd240efdc3233db76981237277778939eaf11
inspizzz <wiktor.wiejak@gmail.com>
2025-08-14 16:17:06 +0100

    Update wiktor.py

--- wiktor.py
+++ wiktor.py
@@ -1,4 +1,13 @@
 from datetime import datetime
 
-def foo(param):
-  print(f"Current time: {datetime.now()}")
+class wiktors_class:
+  def __init__(self, param1):
+    self.param1 = param1
+  def get_time(self):
+    return datetime.now()
+  def get_param(self):
+    return self.param1
+  def run(self, param):
+    print(f"run function executed with parameter {param}")
+    print(self.get_time())
+    print(self.get_param())
